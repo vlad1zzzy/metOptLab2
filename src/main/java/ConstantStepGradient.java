357aefd55446c42e1b0293b67a8d1c6cba4dd7ed
Dmitry <284629@niuitmo.ru>
2021-04-07 20:53:08 +0300

    fixed

--- src/main/java/ConstantStepGradient.java
+++ src/main/java/ConstantStepGradient.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class ConstantStepGradient {
-    public void gradient(double[] x, QuadraticFunction function, double epsilon, int a, int b) {
-        double lambda = function.findMin(a, b, epsilon, x);
-        double[] xk;
+    public static void gradient(double[] xk, QuadraticFunction function, double epsilon, int a, int b) {
+        double lambda = function.findMin(a, b, epsilon, xk);
+        double[] x = xk;
         do {
             xk = function.reducedAddVectors(x, function.findGradient(x, -1), lambda);
             if (function.findFx(xk) < function.findFx(x)) {
@@ -12,6 +12,7 @@ public class ConstantStepGradient {
             } else {
                 lambda /= 2;
             }
+            System.out.println(function.dfNormalize(xk));
         } while (function.dfNormalize(xk) > epsilon);
         System.out.println(Arrays.stream(xk).mapToObj(String::valueOf)
                 .collect(Collectors.joining(", ", "ANSWER : f( ", " ) = ")) + function.findFx(xk));
--- src/main/java/Gradient.java
+++ src/main/java/Gradient.java
@@ -2,13 +2,14 @@
 public class Gradient {
     public static double EPS = 0.00001;
 
-    public static double[][] a = {{4, 0}, {0, 1}};
-    public static double[] b = {4, -6};
-    public static double c = 10;
+    public static double[][] a = {{64, 126}, {126, 64}};
+    public static double[] b = {-10, 30};
+    public static double c = 13;
 
     public static void main(String[] args) {
         QuadraticFunction function = new QuadraticFunction(a, b, c);
         GradientGreatDescent.gradient(new double[]{10, 10}, function, EPS, -10, 10);
         ConjugateGradient.gradient(new double[]{10, 10}, function, EPS, -10, 10, 2);
+        ConstantStepGradient.gradient(new double[]{10, 10}, function, EPS, -10, 10);
     }
 }
