6e4aaa8043c4cd546575d91b549ea3686697135d
Dmitry <284629@niuitmo.ru>
2021-04-07 21:03:29 +0300

    fixed

--- src/main/java/Gradient.java
+++ src/main/java/Gradient.java
@@ -1,13 +1,12 @@
 
 public class Gradient {
-    public static double EPS = 0.00001;
-
-    public static double[][] a = {{64, 126}, {126, 64}};
-    public static double[] b = {-10, 30};
-    public static double c = 13;
+    public static double EPS = 0.0001;
 
     public static void main(String[] args) {
-        QuadraticFunction function = new QuadraticFunction(a, b, c);
+        QuadraticFunction function1 = new QuadraticFunction(new double[][]{{64, 126}, {126, 64}}, new double[]{-10, 30}, 13);
+        QuadraticFunction function2 = new QuadraticFunction(new double[][]{{254, 506}, {506, 254}}, new double[]{50, 130}, -111);
+        QuadraticFunction function3 = new QuadraticFunction(new double[][]{{211,-420},{-420,211}},new double[]{-192,50}, -25);
+        QuadraticFunction function = function3;
         GradientGreatDescent.gradient(new double[]{10, 10}, function, EPS, -10, 10);
         ConjugateGradient.gradient(new double[]{10, 10}, function, EPS, -10, 10, 2);
         ConstantStepGradient.gradient(new double[]{10, 10}, function, EPS, -10, 10);
