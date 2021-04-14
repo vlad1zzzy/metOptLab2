b2a556e89a40bd8fdc17ddf4c2131c062e7ed43d
Dmitry <284629@niuitmo.ru>
2021-04-14 22:41:49 +0300

    fixed

--- src/main/java/QuadraticFunctionFactory.java
+++ src/main/java/QuadraticFunctionFactory.java
@@ -1,17 +1,29 @@
 import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class QuadraticFunctionFactory {
     public static QuadraticFunctionDiagonalised randomD(int n, double maxX) {
         Random random = new Random();
-        double minX = 1;
+        double minX = 2;
+        maxX--;
         double[] a = new double[n];
         double[] b = new double[n];
         double c = 0;
         for (int j = 0; j < n; j++) {
             b[j] = minX + (maxX - minX) * random.nextDouble();
-            a[j] = minX + (maxX - minX) * random.nextDouble();
         }
-        a[n - 1] = maxX;
+        Set<Double> set = new TreeSet<>();
+        set.add(1.0);
+        set.add(maxX + 1);
+        while (set.size() != n) {
+            set.add(minX + (maxX - minX) * random.nextDouble());
+        }
+        int i = 0;
+        for (Double val : set) {
+            a[i] = val;
+            i++;
+        }
         return new QuadraticFunctionDiagonalised(a, b, c);
     }
 
